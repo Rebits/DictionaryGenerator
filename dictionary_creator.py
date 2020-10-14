d0add9e9cb1632e2268f604f1c634049bf721f87
Olivia Marín <olivia.marin.delgado95@gmail.com>
2020-10-15 00:04:42 +0200

    Print languages in same loop

--- dictionary_creator.py
+++ dictionary_creator.py
@@ -7,18 +7,13 @@ from googletrans import Translator
 word_list = input("Introduce your word list, separated with ,\n\n").split(",")
 translator = Translator()
 
-
 for word in  word_list:
     translation = translator.translate(
     word, src="es", dest="pt").text
     print(translation)
-
-for word in  word_list:
     translation = translator.translate(
     word, src="es", dest="fr").text
     print(translation)
-
-for word in  word_list:
     translation = translator.translate(
     word, src="es", dest="en").text
     print(translation)
