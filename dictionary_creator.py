8b1dee5cc8e048ee61986cbcbd501589f476cfc7
Olivia Marín <olivia.marin.delgado95@gmail.com>
2020-10-15 17:44:11 +0200

    Reading from a input file. Output in new file "translation.txt".

--- dictionary_creator.py
+++ dictionary_creator.py
@@ -5,15 +5,22 @@ from googletrans import Translator
 origin_languages = ["es"]
 destination_languages = ["pt","fr","en"]
 translator = Translator()
+translated_file = open("translation.txt","w")
 
-file = open("hello.txt", "r") 
-for line in file.readlines():
+text = input("Please enter the name of the file with the extension:\n")
+print(f'You entered {text}')
 
-# for word in  word_list:
+file = open(text, "r") 
+temp = file.read().splitlines()
+for line in temp:
     for origin in origin_languages:
+        translated_file.writelines(line + "/ " )
         for destination in destination_languages:
             translation = translator.translate(
-               line, src=origin, dest=destination).text
-            print(line + "  ----->   "   +  translation)
+                line, src=origin, dest=destination).text
+            translated_file.writelines(translation + "/ ")
+        translated_file.writelines("\n")
 
-file.close() 
+file.close()
+translated_file.close() 
+
