571cb32de77381fabfab805d66757076f44e1ef6
Víctor Rebollo <victorrebollop@gmail.com>
2020-10-15 15:57:05 +0200

    Fixed indent

--- dictionary_creator.py
+++ dictionary_creator.py
@@ -3,14 +3,14 @@
 from googletrans import Translator
 
 origin_languages = ["es"]
-destination_languages = ["pt","fr"]
+destination_languages = ["pt", "fr"]
 
 word_list = input("Introduce your word list, separated with ,\n\n").split(",")
 translator = Translator()
 
-for word in  word_list:
+for word in word_list:
     for origin in origin_languages:
         for destination in destination_languages:
             translation = translator.translate(
                 word, src=origin, dest=destination).text
-            print(word + "  ----->   "   +  translation)
+            print(word + "  ----->   " + translation)
